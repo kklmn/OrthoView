5e11769fb54fc2df13775485cdfb2125b5e785a4
kklmn <konstantin.klementiev@gmail.com>
2020-02-05 17:39:28 +0100

    reset zoom on perspective switch

--- OrthoView.py
+++ OrthoView.py
@@ -186,6 +186,9 @@ class MyMplCanvas(mpl_qt.FigureCanvasQTAgg):
             self.img.set_data(img)
             self.img.set_extent(
                 [-0.5, img.shape[1]-0.5, img.shape[0]-0.5, -0.5])
+            self.axes.set_xlim((0, img.shape[1]))
+            self.axes.set_ylim((img.shape[0], 0))
+            self.toolbar.update()
         self.draw()
 
     def onPress(self, event):
